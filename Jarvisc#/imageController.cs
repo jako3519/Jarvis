6417dbcf3946d372dd6bd807488a0a995b7850c6
jako3519 <123554744+jako3519@users.noreply.github.com>
2026-09-07 21:50:37 +0200

    huefirsttry

--- Jarvisc#/imageController.cs
+++ Jarvisc#/imageController.cs
@@ -11,13 +11,12 @@ public async Task ConnectAsync()
     var factory = new MqttFactory();
     _mqttClient = factory.CreateMqttClient();
 
-    _mqttClient.ApplicationMessageReceivedAsync += message =>
+        _mqttClient.ApplicationMessageReceivedAsync += async message =>
     {
         var gesture = Encoding.UTF8.GetString(
             message.ApplicationMessage.PayloadSegment
         );
-        HandleGesture(gesture);
-        return Task.CompletedTask;
+        await HandleGesture(gesture);
     };
 
     var options = new MqttClientOptionsBuilder()
@@ -43,8 +42,29 @@ public async Task ConnectAsync()
     Console.WriteLine("Forbundet til Mosquitto!");
 }
 //test
-    private void HandleGesture(string gesture)
+    private async Task HandleGesture(string gesture)
     {
         Console.WriteLine($"Gesture: {gesture} modtaget, c#");
+
+        switch (gesture)
+        {
+          
+        case "Thumb_Up":
+        await _mqttClient.PublishStringAsync(
+        "zigbee2mqtt/0x001788010f815763/set",
+        "{\"state\":\"ON\"}");
+            break;
+
+        case "Thumb_Down":
+            await _mqttClient.PublishStringAsync(
+                "zigbee2mqtt/0x001788010f815763/set",
+                "{\"state\":\"OFF\"}");
+            break;
+
+                
+        }
+
+
+
     }
 }}
